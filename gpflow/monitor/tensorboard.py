cfe10b14f920f9f9ccfc1f8d1b5b1ac3605eb273
Aidan Scannell <scannell.aidan@gmail.com>
2020-08-06 11:34:02 +0100

    Added class ImageWithCbarToTensoboard which can handle color bars.

--- gpflow/monitor/tensorboard.py
+++ gpflow/monitor/tensorboard.py
@@ -25,7 +25,7 @@ from ..utilities import parameter_dict
 from .base import MonitorTask
 
 
-__all__ = ["ToTensorBoard", "ModelToTensorBoard", "ScalarToTensorBoard", "ImageToTensorBoard"]
+__all__ = ["ToTensorBoard", "ModelToTensorBoard", "ScalarToTensorBoard", "ImageToTensorBoard", "ImageWithCbarToTensorBoard"]
 
 
 class ToTensorBoard(MonitorTask):
@@ -203,6 +203,101 @@ class ImageToTensorBoard(ToTensorBoard):
 
         self._clear_axes()
         self.plotting_function(self.fig, self.axes)
+        canvas = FigureCanvasAgg(self.fig)
+        canvas.draw()
+
+        # get PNG data from the figure
+        png_buffer = BytesIO()
+        canvas.print_png(png_buffer)
+        png_encoded = png_buffer.getvalue()
+        png_buffer.close()
+
+        image_tensor = tf.io.decode_png(png_encoded)[None]
+
+        # Write to TensorBoard
+        tf.summary.image(self.name, image_tensor, step=self.current_step)
+
+
+class ImageWithCbarToTensorBoard(ToTensorBoard):
+    def __init__(
+        self,
+        log_dir: str,
+        plotting_function: Callable[
+            ["matplotlib.figure.Figure", "matplotlib.figure.Axes"], "matplotlib.figure.Figure"
+        ],
+        name: Optional[str] = None,
+        *,
+        fig_kw: Optional[Dict[str, Any]] = None,
+        subplots_kw: Optional[Dict[str, Any]] = None,
+        # colorbar_kw: Optional[Dict[str, Any]] = None,
+    ):
+        """
+        :param log_dir: directory in which to store the tensorboard files.
+            Can be a nested: for example, './logs/my_run/'.
+        :param plotting_function: function performing the plotting.
+        :param name: name used in TensorBoard.
+        :params fig_kw: Keywords to be passed to Figure constructor, such as `figsize`.
+        :params subplots_kw: Keywords to be passed to figure.subplots constructor, such as
+            `nrows`, `ncols`, `sharex`, `sharey`. By default the default values
+            from matplotlib.pyplot as used.
+        """
+        super().__init__(log_dir)
+        self.plotting_function = plotting_function
+        self.name = name
+        # self.file_writer = tf.summary.create_file_writer(log_dir)
+        self.name = name
+        self.fig_kw = fig_kw or {}
+        self.subplots_kw = subplots_kw or {}
+        self.cbs = None
+        # self.colorbar_kw = colorbar_kw or {}
+
+        # self.fig = Figure(**self.fig_kw)
+        # if self.subplots_kw != {}:
+        #     self.axes = self.fig.subplots(**self.subplots_kw)
+        # else:
+        #     self.axes = self.fig.add_subplot(111)
+        # # if self.colorbar_kw != {}:
+        # #     self.cbs = self.fig.colorbar(**self.subplots_kw)
+
+        try:
+            from matplotlib.figure import Figure
+        except ImportError:
+            raise RuntimeError("ImageWithCbarToTensorBoard requires the matplotlib package to be installed")
+
+        self.fig = Figure(**self.fig_kw)
+        if self.subplots_kw != {}:
+            self.axes = self.fig.subplots(**self.subplots_kw)
+        else:
+            self.axes = self.fig.add_subplot(111)
+
+    def _clear_axes(self):
+        if isinstance(self.axes, np.ndarray):
+            for ax in self.axes.flatten():
+                ax.clear()
+        else:
+            self.axes.clear()
+
+    def _clear_cbs(self):
+        tf.print('inside clear cbs')
+        if isinstance(self.cbs, np.ndarray):
+            for cb in self.cbs.flatten():
+                # cb.ax.clear()
+                cb.remove()
+        else:
+            # self.cbs.ax.clear()
+            self.cbs.remove()
+
+    def run(self, **unused_kwargs):
+        from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+        self._clear_axes()
+        # if self.colorbar_kw != {}:
+        if self.cbs is not None:
+            tf.print('cbs is not None')
+            self._clear_cbs()
+        self.cbs = self.plotting_function(self.fig, self.axes)
+        # else:
+        #     self.plotting_function(self.fig, self.axes)
         canvas = FigureCanvasAgg(self.fig)
         canvas.draw()
 
